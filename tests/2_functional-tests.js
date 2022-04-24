1e4ffcafbeb9673caa00612cb6ba7251f6714a3e
Edgar Casanova <edgar.cardoz@gmail.com>
2022-04-24 01:09:45 -0500

    upload my results

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Edgar')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Edgar');
           done();
         });
     });
@@ -38,45 +38,68 @@ suite('Functional Tests', function () {
         .put('/travellers')
 
         .end(function (err, res) {
-          assert.fail();
-
+          //assert.equal(err, res);
+          assert.equal(res.status, 200);
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      chai
+        .request(server)
+        .put('/travellers')
 
-      done();
+        .end(function (err, res) {
+          //assert.equal(err, res);
+          assert.equal(res.status, 200);
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
+Browser.site = 'http://localhost:3000';
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
+  const browser = new Browser();
 
-
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
+      assert.property(Browser, 'site');
     });
   });
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo');
+      browser.pressButton('submit', function () {
+        browser.assert.success();
+        browser.assert.text('#name', 'Cristoforo');
+        browser.assert.text('#surname', 'Colombo');
+        browser.assert.element('#dates', 1);
+        done();
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      //assert.fail();
 
-      done();
+      //done();
+      browser.fill('surname', 'Vespucci');
+      browser.pressButton('submit', function () {
+        browser.assert.success();
+        browser.assert.text('#name', 'Amerigo');
+        browser.assert.text('#surname', 'Vespucci');
+        browser.assert.element('#dates', 1);
+        done();
+      });
     });
   });
 });
